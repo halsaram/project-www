64110dce18680480f07f12227c5f1259a1d885e4
Lisa <j.romany91@gmail.com>
2019-10-11 17:01:25 +0900

    Revert "no message"
    This reverts commit 1f394a18dcbce87c04664e405371ee215b42313f.

--- pages/collections/index.js
+++ pages/collections/index.js
@@ -1,21 +1,9 @@
-/**************************************************************************************
- * 제목          : collections
- * 소스파일 이름  : pages/collections/index.js
- * 파일설명		   : 모든 collections 페이지를 연결시킨다
- * 작성자		     : 전새희
- * 버전		      : 1.0.0
- * 생성일자		   : 2019-10-11
- * 최종수정일자 	: 2019-10-11
- * 최종수정자	   : 전새희
- * 최종수정내용	  : collections 페이지 연결
-**************************************************************************************/
-
 import Layout from '../../components/Layout'
 import Creative from './creative'
 import Brand from './brands'
 import Campaign from './campaigns'
 
-const CollectionPage = (props) =>
+const CollectionPageContent = (props) =>
     <div>
         {props.url.query.id == 'creative' && <Creative />}
         {props.url.query.id == 'brand' && <Brands />}
@@ -24,6 +12,6 @@ const CollectionPage = (props) =>
 
 export default (props) => (
     <Layout title={props.url.query.title} >
-        <CollectionPage url={props.url} />
+        <CollectionPageContent url={props.url} />
     </Layout>
 )
