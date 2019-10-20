02cadf8b3d286b14210613af5f9555de1547a317
kumjungmin <rmawjdals@naver.com>
2019-10-20 14:33:10 +0900

    .

--- pages/project/Agreements.js
+++ pages/project/Agreements.js
@@ -11,6 +11,7 @@
 **************************************************************************************/
 
 import Link from 'next/link';
+import { Checkbox, Grid, Segment  } from 'semantic-ui-react'
 
 const ProjectLink = (props) => (
     <Link as='/p' href={`/project?id=${props.id}&title=프로젝트개요`}>
@@ -19,42 +20,73 @@ const ProjectLink = (props) => (
 )
 
 const Agreements = () => (
-    <div className="ProjectStartAgreements__ContentsBody-sc-1vrd6a2-1 hMCHfF" data-reactid="41">
-        <h2 className="ProjectStartAgreements__Header-sc-1vrd6a2-2 gAoVXO" data-reactid="42"># 프로젝트를 올리기 전에 확인해 주세요.</h2>
-        <div className="ProjectStartAgreements__Guidance-sc-1vrd6a2-3 ipkmcm" data-reactid="43">
-            텀블벅은 공개 검토 절차를 통해 창작자님이 작성한 프로젝트가 텀블벅의 정책을 준수하는지 확인하고 있습니다. <br data-reactid="45" />
-            아래 사항들을 확인한 후 프로젝트를 올려 주세요.
-            <ul data-reactid="47">
-                <li data-reactid="48"><a href="https://help.tumblbug.com/hc/ko/articles/115006302047" target="_blank" rel="noopener noreferrer" data-reactid="49">공개 검토 절차 및 기준</a></li>
-                <li data-reactid="50"><a href="https://help.tumblbug.com/hc/ko/articles/115007487228" target="_blank" rel="noopener noreferrer" data-reactid="51">창작자 자격 요건</a></li>
-            </ul>
-        </div>
-        <form autocomplete="on" data-reactid="52">
-            <ul data-reactid="53">
-                <li data-reactid="54">
-                    <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="55">
-                        <input type="checkbox" id="2871e" required="" data-reactid="56" value='on'/><label for="2871e" data-reactid="57">새로운 아이디어를 실현하기 위한 프로젝트입니다.</label>
-                    </div>
-                </li>
-                <li data-reactid="58">
-                    <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="59">
-                        <input type="checkbox" id="ceec9" required="" data-reactid="60" /><label for="ceec9" data-reactid="61">이미 시판된 제품, 현금이나 지분 등 수익 제공, 선물 없는 단순 기부, 성인 인증이 필요한 콘텐츠 등 기준에 맞지 않는 선물을 제공하지 않습니다.</label>
-                    </div>
-                </li>
-                <li data-reactid="62">
-                    <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="63">
-                        <input type="checkbox" id="5f0fa" required="" data-reactid="64" /><label for="5f0fa" data-reactid="65">창작자의 신분증 혹은 사업자등록증, 국내 은행 계좌, 연락 가능한 본인 휴대폰 번호가 있습니다.</label>
-                    </div>
-                </li>
-                <li data-reactid="66">
-                    <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="67">
-                        <input type="checkbox" id="679fd" required="" data-reactid="68" /><label for="679fd" data-reactid="69">창작자 대표자는 19세 이상 성인입니다. (미성년자 참여 방법은 자격 요건을 참고해 주세요)</label>
-                    </div>
-                </li>
-            </ul>
-            <ProjectLink id='summary' title='계속하기' />
-        </form>
+    <div>
+        <Grid columns='equal'>
+            <Grid.Column /> 
+            <Grid.Column width={8}>
+                <Checkbox label='펀딩 진행 중에는 제공할 리워드를 다른 온라인, 오프라인에서 펀딩하거나 판매하지 않습니다.' /><br/>
+                <Checkbox label='제공할 리워드는 현금, 지분 등의 수익이 아닌 제품 또는 서비스입니다.' /><br/>
+                <Checkbox label='진행할 프로젝트가 지적 재산권을 침해하지 않습니다.' /><br/>
+                <Checkbox label='서포터에게 프로젝트 진행 과정을 안내하고, 배송 약속을 지킬 수 있습니다.' /><br/>
+                <Checkbox label='서포터와의 신뢰를 위한 펀딩 진행, 제품 제작, 배송 등 모든 과정에서 겪는 어려움들을 서포터에게 진솔하게 전달하고 문제를 해결합니다.' /><br/>
+                <Checkbox label='해당 프로젝트 종료 후 서포터들의 신고 누적시 정산에 패널티를 부여합니다.' /><br/>
+            </Grid.Column>
+            <Grid.Column/>
+        </Grid>
+
+
+
+        
     </div>
+
+
+
+
+
+
+
+
+
+  
 );
 
 export default Agreements;
+
+
+
+  // <div className="ProjectStartAgreements__ContentsBody-sc-1vrd6a2-1 hMCHfF" data-reactid="41">
+    //     <h2 className="ProjectStartAgreements__Header-sc-1vrd6a2-2 gAoVXO" data-reactid="42"># 프로젝트를 올리기 전에 확인해 주세요.</h2>
+    //     <div className="ProjectStartAgreements__Guidance-sc-1vrd6a2-3 ipkmcm" data-reactid="43">
+    //         텀블벅은 공개 검토 절차를 통해 창작자님이 작성한 프로젝트가 텀블벅의 정책을 준수하는지 확인하고 있습니다. <br data-reactid="45" />
+    //         아래 사항들을 확인한 후 프로젝트를 올려 주세요.
+    //         <ul data-reactid="47">
+    //             <li data-reactid="48"><a href="https://help.tumblbug.com/hc/ko/articles/115006302047" target="_blank" rel="noopener noreferrer" data-reactid="49">공개 검토 절차 및 기준</a></li>
+    //             <li data-reactid="50"><a href="https://help.tumblbug.com/hc/ko/articles/115007487228" target="_blank" rel="noopener noreferrer" data-reactid="51">창작자 자격 요건</a></li>
+    //         </ul>
+    //     </div>
+    //     <form autocomplete="on" data-reactid="52">
+    //         <ul data-reactid="53">
+    //             <li data-reactid="54">
+    //                 <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="55">
+    //                     <input type="checkbox" id="2871e" required="" data-reactid="56" value='on'/><label for="2871e" data-reactid="57">새로운 아이디어를 실현하기 위한 프로젝트입니다.</label>
+    //                 </div>
+    //             </li>
+    //             <li data-reactid="58">
+    //                 <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="59">
+    //                     <input type="checkbox" id="ceec9" required="" data-reactid="60" /><label for="ceec9" data-reactid="61">이미 시판된 제품, 현금이나 지분 등 수익 제공, 선물 없는 단순 기부, 성인 인증이 필요한 콘텐츠 등 기준에 맞지 않는 선물을 제공하지 않습니다.</label>
+    //                 </div>
+    //             </li>
+    //             <li data-reactid="62">
+    //                 <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="63">
+    //                     <input type="checkbox" id="5f0fa" required="" data-reactid="64" /><label for="5f0fa" data-reactid="65">창작자의 신분증 혹은 사업자등록증, 국내 은행 계좌, 연락 가능한 본인 휴대폰 번호가 있습니다.</label>
+    //                 </div>
+    //             </li>
+    //             <li data-reactid="66">
+    //                 <div className="_13KHfN73YmQgsYHxXvuh_J _3bQlo02zZkYjUBehiAWUDX ProjectStartAgreements__ServiceAgreementCheckbox-sc-1vrd6a2-4 iQcbCy" data-reactid="67">
+    //                     <input type="checkbox" id="679fd" required="" data-reactid="68" /><label for="679fd" data-reactid="69">창작자 대표자는 19세 이상 성인입니다. (미성년자 참여 방법은 자격 요건을 참고해 주세요)</label>
+    //                 </div>
+    //             </li>
+    //         </ul>
+    //         <ProjectLink id='summary' title='계속하기' />
+    //     </form>
+    // </div>
