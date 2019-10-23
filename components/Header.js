84d347aa963c9743cede59ec4f0837a0176b4b52
hwisun <o1031436831@gmail.com>
2019-10-23 14:02:02 +0900

    update server.js && add routes.js

--- components/Header.js
+++ components/Header.js
@@ -1,102 +1,27 @@
 import Link from 'next/link';
-import { Header, Icon, Image, Input, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
 
 
-const HeaderBar =()=> (
-  
-<div>
+const HeaderBar = () => (
+  <div>
+    <Menu fluid widths={4}>
+      <Menu.Item>
+        <Link href='/'>
+          <a>홈</a>
+        </Link>
+      </Menu.Item>
 
-
-        <Menu fluid widths={4}>
-          <Menu.Item>
-            <Link href='/'>
-              <a>홈</a>
-            </Link>
-          </Menu.Item>
-        
-        <Menu.Item
-          name='카테고리'
-        />
-        <Menu.Item
-          name='새로운 프로젝트'
-        />
-        <Menu.Item
-          name='성공임박 프로젝트'
-        />
-
-      </Menu>
-      </div>
-
-
-      
+      <Menu.Item
+        name='카테고리'
+      />
+      <Menu.Item
+        name='새로운 프로젝트'
+      />
+      <Menu.Item
+        name='성공임박 프로젝트'
+      />
+    </Menu>
+  </div>
 )
 
-
-
-
-
-
-
-
-
-
-// const HeaderBar = () => (
-//     <div>
-//         <div className="ScrollTop__ScrollTopBtnWrapper-j9uqn7-0 cKbfhA" data-reactid="3">
-//             <button className="ScrollTop__ScrollTopBtn-j9uqn7-1 bcbYyv" data-reactid="4"><span data-reactid="5">최상위로</span></button>
-//             <i className="_3EDOT6mnWfo2CobuYD5UO7 _2PY6DpDelT9jvKKFjCr7gd _14AOh5T6DdcgeGZ5YUV9uR _1QY7TzdLHKX3-BKPDNNYKF" data-reactid="6"></i>
-//         </div>
-//         <div data-reactid="7">
-//             <div className="SiteHeader__SiteHeaderWrapper-q8dvod-0 iSuAdB" data-reactid="8">
-//                 <div className="Header-ou4e17-0 cwcqcZ" data-reactid="9">
-//                     <div className="Container-gci8y7-0 MskhC" data-reactid="10">
-//                         <div className="SiteHeader__SiteHeaderMenu-q8dvod-1 fMekoE" data-reactid="11">
-//                             <div>
-//                                 <Link href='/'><a>
-//                                     <Header as='h3'>
-//                                         <Icon name='home' />
-//                                         <Header.Content>Halsaram</Header.Content>
-//                                     </Header>
-//                                 </a></Link>
-//                             </div>
-//                             <div className="SiteHeader__LeftMenu-q8dvod-2 fbsOED" data-reactid="12">
-//                                 <button className="SiteHeader__LinkButton-q8dvod-7 hLwNNy" data-reactid="13">
-//                                     <Icon name='bars' />
-//                                     <span className="SiteHeader__ItemLabel-q8dvod-8 hWygls" data-reactid="15">프로젝트 둘러보기</span>
-//                                 </button>
-//                                 <span className="SiteHeader__ForMobile-q8dvod-9 ihCbfB" data-reactid="16">
-//                                     <Link as='/p' href='/project?id=start&title=프로젝트올리기'>
-//                                         <a className="SiteHeader__LinkItem-q8dvod-6 dALTNz" data-reactid="17">
-//                                             <span className="SiteHeader__ItemLabel-q8dvod-8 hWygls" data-reactid="18">프로젝트 올리기</span>
-//                                         </a>
-//                                     </Link>
-//                                 </span>
-//                             </div>
-//                             {/* <div className="SiteHeader__CenterMenu-q8dvod-3 hbKGuE" data-reactid="19">
-//                                 <a className="SiteHeader__ButtonLink-q8dvod-5 efdMbN" href="https://halsaram.com/" data-reactid="20">
-//                                     <div className="TumblbugLogo-sc-5rx33z-0 iDsnca" data-reactid="21">
-//                                         <svg id="halsaram_logo" viewbox="0 0 107.89 28.39" data-reactid="22">
-//                                             <title data-reactid="23">Halsaram</title>
-//                                             <p>halsaram</p>
-//                                         </svg>
-//                                     </div>
-//                                 </a>
-//                             </div> */}
-//                             <div className="SiteHeader__RightMenu-q8dvod-4 cLNJqs" data-reactid="33">
-//                                 <button className="SiteHeader__SearchButton-q8dvod-13 hmmaSW Button-sc-1x93b2b-0 eFQBDO" data-reactid="34"></button>
-//                                 <Link as='/log' href= '/login?id=login&title=로그인'>
-//                                     <a className="q8dvod-5-SiteHeader__ButtonLink-ktApHV kGropT"  data-reactid="35">
-//                                         <span className="SiteHeader__ItemLabel-q8dvod-8 hWygls" data-reactid="36">로그인 / 회원가입</span>
-//                                         <Image circular src='http://placehold.it/40x40' />
-//                                     </a>
-//                                 </Link>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   );
-  
-  export default HeaderBar;
+export default HeaderBar;
--- components/Layout.js
+++ components/Layout.js
@@ -113,41 +113,6 @@ export default class Layout extends React.Component {
     return (
       <React.Fragment>
         <Head>
-          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
-          <meta name="description" content="모든 사람의 창조적인 시도를 위한 크라우드펀딩 플랫폼" />
-          <meta data-react-helmet="true" property="fb:app_id" content="200842993269405" />
-          <meta data-react-helmet="true" property="og:site_name" content="tumblbug" />
-          <meta data-react-helmet="true" property="og:type" content="website" />
-          <meta data-react-helmet="true" property="og:image" content="https://tumblbug-assets.imgix.net/brand/tumblbug_introduction.png" />
-          <meta data-react-helmet="true" property="og:image:width" content="620" />
-          <meta data-react-helmet="true" property="og:image:height" content="465" />
-          <meta data-react-helmet="true" property="og:url" content="https://www.tumblbug.com/" />
-          <meta data-react-helmet="true" property="og:title" content="텀블벅 tumblbug" />
-          <meta data-react-helmet="true" property="og:description" content="모든 사람의 창조적인 시도를 위한 크라우드펀딩 플랫폼" />
-          <meta data-react-helmet="true" name="twitter:site" content="tumblbug" />
-          <meta data-react-helmet="true" name="twitter:creator" content="tumblbug" />
-          <meta data-react-helmet="true" name="twitter:card" content="summary_large_image" />
-          <meta data-react-helmet="true" name="twitter:image" content="https://tumblbug-assets.imgix.net/brand/tumblbug_introduction.png" />
-          <meta data-react-helmet="true" name="twitter:url" content="https://www.tumblbug.com/" />
-          <meta data-react-helmet="true" name="twitter:title" content="텀블벅 tumblbug" />
-          <meta data-react-helmet="true" name="twitter:description" content="모든 사람의 창조적인 시도를 위한 크라우드펀딩 플랫폼" />
-          
-          <link rel="dns-prefetch" href="https://tumblbug-assets.imgix.net/" />
-          <link rel="dns-prefetch" href="https://tumblbug-pci2.imgix.net/" />
-          <link rel="dns-prefetch" href="https://tumblbug-psi.imgix.net/" />
-          <link rel="dns-prefetch" href="https://tumblbug-upi.imgix.net/" />
-          <link rel="dns-prefetch" href="https://www.google-analytics.com/" />
-          <link rel="dns-prefetch" href="https://developers.kakao.com/" />
-          <link rel="dns-prefetch" href="https://d2om2e6rfn032x.cloudfront.net/" />
-          <link rel="dns-prefetch" href="https://qysoaxc73e-dsn.algolia.net/" />
-          <link rel="dns-prefetch" href="https://stats.g.doubleclick.net/" />
-          <script type="text/javascript" async="" src="../static/js/linkid.js" />
-          <script type="text/javascript" async="" src="../static/js/analytics.js" />
-          <script type="text/javascript" async="" src="../static/js/analytics.min.js" />
-          <script type="text/javascript" async="" src="../static/js/kakao.min.js" />
-          {/* <script type="text/javascript" async="" src="../static/js/application.js" /> */}
-          
           <link
             rel="stylesheet"
             href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
@@ -179,16 +144,7 @@ export default class Layout extends React.Component {
             <Link as='/p' href= '/project?id=start&title=프로젝트올리기'>
               <a>프로젝트 올리기</a>
             </Link>
-          </Menu.Item>
-
-            {/* 데이터 현황 */}
-            <div>
-              <strong>onChange:</strong>
-              <pre>{JSON.stringify({ search }, null, 1)}</pre>
-              <strong>onSubmit:</strong>
-              <pre>{JSON.stringify({ submittedSearch }, null, 1)}</pre>
-            </div>
-         
+          </Menu.Item>        
         </Menu.Menu>
       </Menu>
 
