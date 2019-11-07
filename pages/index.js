197a9bc492b9aa993d9d4a0edee6bc60b0bc48e3
kumjungmin <rmawjdals@naver.com>
2019-11-07 13:50:54 +0900

    main change

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@
 **************************************************************************************/
 import React, {Component} from 'react'
 import Page from '../components/Page'
-
-import Layout from '../components/Layout'
+import { Grid, Segment } from 'semantic-ui-react'
 import Main_Banner from './main/Main_Banner'
-import Lower_Banner from './main/Lower_Banner'
 import MainList from './main/MainList'
 import CatoList from './main/CatoList'
 import HeaderBar from '../components/Header'
+import { inherits } from 'util'
+import { height } from 'window-size'
 // 임시 데이터------------------------------------------------------------------------
 //map반복문을 통해 a,b,c에 적절한 인덱스의 데이터를 담음
 const EditReco = { //반복문을 써서 9개의 데이터를 가져옴 -현재는 3개만 가져오기로
@@ -40,22 +40,37 @@ const ReReco = { //반복문을 써서 9개의 데이터를 가져옴 -현재는
   b : {name:"", editor:"", day:"", money:"", percent:"30"},
   c : {name:"", editor:"", day:"", money:"", percent:"30"}
 }
-//--------------------------------------------------------------------------
 
+
+
+//--------------------------------------------------------------------------
+const ItemMargin = {    //간격용 디자인
+  margin : 50,
+  width : 100,
+  height : 100
+}
+const CatoColor = {     //카테고리 부분의 뒷 배경
+
+}
 
 const Contents = (props) => (
   <div>
     <HeaderBar/>
     <Main_Banner />
     <div>
-      <MainList value={NewReco} MainTitle='신규 프로젝트'/><br /> <br /><br /><br />
-      <MainList value={EndReco} MainTitle='성공임박 프로젝트'/><br /> <br /><br /><br />
-      <br />
+      <MainList value={NewProject} MainTitle='신규 프로젝트'/>
+      <div style={ItemMargin}></div>      {/* 간격을 margin으로 처리한다. */}
+      
+      <MainList value={SuccessProject} MainTitle='성공임박 프로젝트'/>
+      <div style={ItemMargin}></div>      {/* 간격을 margin으로 처리한다. */}
+
     <div className = "GrayDiv">
       <CatoList />
-    </div><br /> <br /><br /><br />
-    <MainList value={ReReco} MainTitle='앵콜 프로젝트'/>
-    <br /><br /><br />
+    </div>
+    <div style={ItemMargin}></div>      {/* 간격을 margin으로 처리한다. */}
+
+    <MainList value={EncoreProject} MainTitle='앵콜 프로젝트'/>
+    <div style={ItemMargin}></div>      {/* 간격을 margin으로 처리한다. */}
     </div>
   </div>
 );
@@ -74,3 +89,102 @@ export default class extends Component {
     )
   }
 }
+
+//-------------------임시데이터------------------------------------
+//프로젝트별 제목(title) | 창작자(editor) | 디데이(Dday) | 분류(catogory)
+//목표금액(targetCoin) | 모금된 금액(fundCoin)
+//const NewProject = [[a:1,b:2,3],[2],[3]]
+const NewProject = [
+    { id:0, text: '첫번째 일정 입니다.', done: true},
+    { id:1, text: '두번째 일정 입니다.', done: false}
+]
+ //변수에 담고 -> 
+
+// const NewProject = {
+//   one : {title : "첫번째 프로젝트 제목", 
+//         editor : "창작자1", 
+//         Dday : 130, 
+//         catogory : "생활", 
+//         targetCoin :10000, 
+//         fundCoin: 5000 },}
+
+//   two : {title : "두번째 프로젝트 제목", editor : "창작자2", Dday : 50,
+//         catogory : "애완", targetCoin :9000, fundCoin: 5000 },
+//   three : {title : "세번째 프로젝트 제목", editor : "창작자3", Dday : 600,
+//         catogory : "여행", targetCoin :10000, fundCoin: 5000 },
+//   four : {title : "네번째 프로젝트 제목", editor : "창작자4", Dday : 70,
+//         catogory : "생활", targetCoin :500000, fundCoin: 5000 },
+//   five : {title : "다섯번째 프로젝트 제목", editor : "창작자5", Dday : 450,
+//         catogory : "생활", targetCoin :100000, fundCoin: 5000 },
+//   six : {title : "여섯번째 프로젝트 제목", editor : "창작자6", Dday : 600,
+//           catogory : "유아", targetCoin :7000, fundCoin: 2000 },
+//   seven : {title : "일곱번째 프로젝트 제목", editor : "창작자7", Dday : 50,
+//           catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+//   eight :  {title : "여덟번째 프로젝트 제목", editor : "창작자8", Dday : 50,
+//           catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+//   nine : {title : "아홉번째 프로젝트 제목", editor : "창작자9", Dday : 50,
+//           catogory : "생활", targetCoin :10000, fundCoin: 5000 }
+// }
+const SuccessProject = {
+  one : {title : "첫번째 프로젝트 제목", editor : "창작자1", Dday : 130, 
+      catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  two : {title : "두번째 프로젝트 제목", editor : "창작자2", Dday : 50,
+      catogory : "애완", targetCoin :9000, fundCoin: 5000 },
+  three : {title : "세번째 프로젝트 제목", editor : "창작자3", Dday : 600,
+      catogory : "여행", targetCoin :10000, fundCoin: 5000 },
+  four : {title : "네번째 프로젝트 제목", editor : "창작자4", Dday : 70,
+      catogory : "생활", targetCoin :500000, fundCoin: 5000 },
+  five : {title : "다섯번째 프로젝트 제목", editor : "창작자5", Dday : 450,
+      catogory : "생활", targetCoin :100000, fundCoin: 5000 },
+  six : {title : "여섯번째 프로젝트 제목", editor : "창작자6", Dday : 600,
+      catogory : "유아", targetCoin :7000, fundCoin: 2000 },
+  seven : {title : "일곱번째 프로젝트 제목", editor : "창작자7", Dday : 50,
+      catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  eight :  {title : "여덟번째 프로젝트 제목", editor : "창작자8", Dday : 50,
+      catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  nine : {title : "아홉번째 프로젝트 제목", editor : "창작자9", Dday : 50,
+      catogory : "생활", targetCoin :10000, fundCoin: 5000 }
+
+}
+const CatoProject = {
+  one : {title : "첫번째 프로젝트 제목", editor : "창작자1", Dday : 130, 
+        catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  two : {title : "두번째 프로젝트 제목", editor : "창작자2", Dday : 50,
+        catogory : "애완", targetCoin :9000, fundCoin: 5000 },
+  three : {title : "세번째 프로젝트 제목", editor : "창작자3", Dday : 600,
+        catogory : "여행", targetCoin :10000, fundCoin: 5000 },
+  four : {title : "네번째 프로젝트 제목", editor : "창작자4", Dday : 70,
+        catogory : "생활", targetCoin :500000, fundCoin: 5000 },
+  five : {title : "다섯번째 프로젝트 제목", editor : "창작자5", Dday : 450,
+        catogory : "생활", targetCoin :100000, fundCoin: 5000 },
+  six : {title : "여섯번째 프로젝트 제목", editor : "창작자6", Dday : 600,
+        catogory : "유아", targetCoin :7000, fundCoin: 2000 },
+  seven : {title : "일곱번째 프로젝트 제목", editor : "창작자7", Dday : 50,
+        catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  eight :  {title : "여덟번째 프로젝트 제목", editor : "창작자8", Dday : 50,
+        catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  nine : {title : "아홉번째 프로젝트 제목", editor : "창작자9", Dday : 50,
+        catogory : "생활", targetCoin :10000, fundCoin: 5000 }
+
+}
+const EncoreProject = {
+  one : {title : "첫번째 프로젝트 제목", editor : "창작자1", Dday : 130, 
+          catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  two : {title : "두번째 프로젝트 제목", editor : "창작자2", Dday : 50, 
+          catogory : "애완", targetCoin :9000, fundCoin: 5000 },
+  three : {title : "세번째 프로젝트 제목", editor : "창작자3", Dday : 600,
+          catogory : "여행", targetCoin :10000, fundCoin: 5000 },
+  four : {title : "네번째 프로젝트 제목", editor : "창작자4", Dday : 70,
+          catogory : "생활", targetCoin :500000, fundCoin: 5000 },
+  five : {title : "다섯번째 프로젝트 제목", editor : "창작자5", Dday : 450,
+          catogory : "생활", targetCoin :100000, fundCoin: 5000 },
+  six : {title : "여섯번째 프로젝트 제목", editor : "창작자6", Dday : 600,
+         catogory : "유아", targetCoin :7000, fundCoin: 2000 },
+  seven : {title : "일곱번째 프로젝트 제목", editor : "창작자7", Dday : 50,
+          catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  eight :  {title : "여덟번째 프로젝트 제목", editor : "창작자8", Dday : 50,
+          catogory : "생활", targetCoin :10000, fundCoin: 5000 },
+  nine : {title : "아홉번째 프로젝트 제목", editor : "창작자9", Dday : 50,
+          catogory : "생활", targetCoin :10000, fundCoin: 5000 }
+
+} 
--- pages/main/MainList.js
+++ pages/main/MainList.js
@@ -11,37 +11,114 @@
 **************************************************************************************/
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
 import { Card, Icon, Grid, Segment, Button, Progress, Pagination } from 'semantic-ui-react'
+import Page from '../../components/Page';
 
 const extra = (
   <div>
     <Icon name='user' />
-
     <Progress percent="33" size='tiny' indicating />
   </div>
 )
 
+// 1번째, props.value[0] -> 한 줄의 데이터 -> 배열데이터에 넣고 -> 이용?(mapping)
 
-const MainList = (props) => (
+const MainList = (props) => {
+    const [activePage ,setActivePage] = useState(1);   //현재 활성화된 페이지값
+    const handlePaginationChange = (e, { activePage }) => setActivePage(activePage)
+    // 버튼 클릭시 -> activePage에 값이 변경 -> state변수에 저장
+    const dataList = props.value;
+
+
+
+
+    return(
     <div>
         <Grid>
-            <Grid.Column width="2" />    
-            <Grid.Column width="8">        
-                <h3>{props.MainTitle}</h3>
+            <Grid.Column width="2" />      
+            <Grid.Column width="8">     
+                {/* 프로젝트 제목 */}   
+                <h3>{props.MainTitle}</h3>    
             </Grid.Column>
+
+            {/* 페이지네이션 */}
             <Grid.Column>
-                <Pagination defaultActivePage={1}
-                firstItem={null} lastItem={null}
-                pointing secondary totalPages={4} />
+                <Pagination 
+                activePage={activePage}
+                onPageChange={handlePaginationChange}
+                defaultActivePage={1}
+                firstItem={null}
+                lastItem={null} 
+                pointing secondary totalPages={3}  
+                 />     
             </Grid.Column>
         </Grid>
+        
          <br/>
+      
+        
         <Grid columns='equal'>
             <Grid.Row centered>
+               {/* 1. 9개 가지고 옴-> */}
+               {/* 데이터값 props.value */}
+                {/*페이지클릭 -> activePage(1,2,3)변수값에 저장  */}
+                {/* 1 클릭시 -> 데이터 1~3까지 */}
+                {/* 2 클릭시 -> 데이터 4~6까지 */}
+                {/* 3 클릭시 -> 데이터 7~9까지 */}
+
+                
+               {/* 첫번째 카드 */}
                 <Grid.Column width="4"> 
-                    <Link as='/상세페이지' href={{ pathname: '/detail', query: { id: 'story', title: '스토리' } }}>
+                    <Card
+                        image='../../static/test/testlist.png'
+                        header={props.MainTitle}
+                        meta='Friend'
+                        description={props.value.id}
+                        extra={extra}
+                        />
+                        
+                </Grid.Column>
+
+                {/* 두번째 카드 */}
+                <Grid.Column width="4"> 
+                    <Card
+                        image='../../static/test/testlist.png'
+                        header={props.MainTitle}
+                        meta='Friend'
+                        description='Elliot is a sound engineer'
+                        extra={extra}
+                        />
+                </Grid.Column>
+
+                {/* 세번쨰 카드 */}
+                <Grid.Column width="4"> 
+                    <Card
+                        image='../../static/test/testlist.png'
+                        header={props.MainTitle}
+                        meta='{dataList[0, id]}'
+                        description='Elliot is a sound engineer'
+                        extra={extra}
+                        />
+                </Grid.Column>
+
+
+               
+               
+               
+{/*                
+                <Grid.Column width="4"> 
+ */}
+
+{/* 
+                {activePage}
+                {/* 1클릭시 -> 데이터 a(1), a+1(2), a+2(3)  시작값 = 1 */} 
+                {/* 2클릭시 -> 데이터 a+2=b(4), b+1(5), b+2(6) 시작값 = 4 */}
+ */}
+
+                
+                    {/* <Link as='/상세페이지' href={{ pathname: '/detail', query: { id: 'story', title: '스토리' } }}>
                         <Card
                             image='../../static/test/testlist.png'
                             header={props.MainTitle}
@@ -67,8 +144,7 @@ const MainList = (props) => (
                     
                 </Grid.Column>
       
-                <Grid.Column width="4">
-                
+                <Grid.Column width="4">   
                     <Card
                         image='../../static/test/testlist.png'
                         header={props.MainTitle}
@@ -77,11 +153,13 @@ const MainList = (props) => (
                         extra={extra}
                         />
                    
-                </Grid.Column>
+                </Grid.Column> */}
       
             </Grid.Row>
         </Grid>
     </div>    
+
+    );
 
      
 
@@ -92,7 +170,7 @@ const MainList = (props) => (
 
   
   
-)
+}
 
 export default MainList
 
